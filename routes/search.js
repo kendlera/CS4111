24b50147be381b1ca7aa062423f606352b9ed5f2
kendlera <kendlera@gmail.com>
2015-03-22 20:52:04 -0400

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 
 /* GET search api. */
-http.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	var query = req.params.query;
-	var response = {
-		e : req.get("http://api.eventful.com/json/events/search?app_key=SGjgGsbhWvJHdRxB&q=music&l=02134")
-	};
-  	res.json(response);
+	var request = require('request');
+	request('http://api.eventful.com/json/events/search?app_key=SGjgGsbhWvJHdRxB&q=' + query + '&l=02134', function(error, response, body) {
+		if(!error && response.statusCode == 200){
+			res.json(body)
+		}
+	})
 });
 
 
 module.exports = router;
+
+
